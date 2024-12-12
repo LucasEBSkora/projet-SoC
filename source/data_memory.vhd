79457c2f371ffe27b0faf128f4df63bd51354cb2
LucasEBSkora <skora.lucas@gmail.com>
2024-12-12 23:32:07 +0100

    Mise a jour data memory pour DM1

--- source/data_memory.vhd
+++ source/data_memory.vhd
@@ -4,30 +4,31 @@ use IEEE.numeric_std.all;
 
 entity data_memory is
     generic (
-        DATA_WIDTH : natural := 32;
-        ADDR_WIDTH : natural := 3
+        BYTE_SIZE : natural := 8;
+        N_BYTES : natural := 4;
+        ADDR_WIDTH : natural := 5
     );
     port (
         clk : in std_logic;
         addr : in natural range 0 to 2 ** ADDR_WIDTH - 1;
-        data : in std_logic_vector((DATA_WIDTH - 1) downto 0);
-        we : in std_logic := '1';
-        q : out std_logic_vector((DATA_WIDTH - 1) downto 0)
+        data : in std_logic_vector((BYTE_SIZE * N_BYTES - 1) downto 0);
+        we : in std_logic_vector((N_BYTES - 1) downto 0) := (others => '0');
+        q : out std_logic_vector((BYTE_SIZE * N_BYTES - 1) downto 0)
     );
 end data_memory;
 
 architecture rtl of data_memory is
     constant MAX_ADDR : natural := 2 ** ADDR_WIDTH - 1;
 
-    subtype word_t is std_logic_vector((DATA_WIDTH - 1) downto 0);
-    type memory_t is array(MAX_ADDR downto 0) of word_t;
+    subtype word_t is std_logic_vector((BYTE_SIZE - 1) downto 0);
+    type memory_t is array(MAX_ADDR * N_BYTES downto 0) of word_t;
 
     function init_ram
         return memory_t is
         variable tmp : memory_t := (others => (others => '0'));
     begin
         for addr_pos in 0 to MAX_ADDR loop
-            tmp(addr_pos) := std_logic_vector(to_unsigned(addr_pos, DATA_WIDTH));
+            tmp(addr_pos) := std_logic_vector(to_unsigned(addr_pos, BYTE_SIZE));
         end loop;
         -- to test lh, lb, lbu and lhu
         -- tmp(0) := X"01020304";
@@ -36,14 +37,24 @@ architecture rtl of data_memory is
     end init_ram;
 
     signal ram : memory_t := init_ram;
+    signal aligned_addr : natural range 0 to 2 ** ADDR_WIDTH - 1;
 begin
+
+    aligned_addr <= to_integer(to_unsigned(addr, ADDR_WIDTH)(ADDR_WIDTH - 1 downto 2) & "00");
+
     process (clk, we, addr, data)
     begin
         if (rising_edge(clk)) then
-            if (we = '1') then
-                ram(addr) <= data;
-            end if;
+            for i in 0 to N_BYTES - 1 loop
+                if (we(i) = '1') then
+                    ram(addr + i) <= data((BYTE_SIZE * (i + 1) - 1) downto BYTE_SIZE * i);
+                end if;
+            end loop;
         end if;
     end process;
-    q <= ram(addr);
+
+    output_assignement :
+    for i in 0 to N_BYTES - 1 generate
+        q((BYTE_SIZE * (i + 1) - 1) downto BYTE_SIZE * i) <= ram(addr + i);
+    end generate;
 end architecture rtl;
--- testbench/data_memory_tb.vhd
+++ testbench/data_memory_tb.vhd
@@ -6,64 +6,92 @@ entity data_memory_tb is
 end entity data_memory_tb;
 
 architecture rtl of data_memory_tb is
-    constant addr_width : natural := 3;
-    constant data_width : natural := 32;
+    constant addr_width : natural := 5;
+    constant byte_size : natural := 8;
+    constant n_bytes : natural := 4;
+    constant data_width : natural := byte_size * n_bytes;
+
+    subtype word_t is std_logic_vector((data_width - 1) downto 0);
+
     component data_memory
         generic (
-            DATA_WIDTH : natural := data_width;
-            ADDR_WIDTH : natural := addr_width
+            BYTE_SIZE : natural := 8;
+            N_BYTES : natural := 4;
+            ADDR_WIDTH : natural := 5
         );
         port (
             clk : in std_logic;
             addr : in natural range 0 to 2 ** ADDR_WIDTH - 1;
-            data : in std_logic_vector((DATA_WIDTH - 1) downto 0);
-            we : in std_logic := '1';
-            q : out std_logic_vector((DATA_WIDTH - 1) downto 0)
+            data : in std_logic_vector((BYTE_SIZE * N_BYTES - 1) downto 0);
+            we : in std_logic_vector((N_BYTES - 1) downto 0) := (others => '0');
+            q : out std_logic_vector((BYTE_SIZE * N_BYTES - 1) downto 0)
         );
     end component;
     signal clk_t : std_logic := '1';
-    signal addr_t : natural range 0 to 7 := 0;
-    signal data_in : std_logic_vector((data_width - 1) downto 0);
-    signal write_enable : std_logic := '0';
-    signal data_out : std_logic_vector((data_width - 1) downto 0);
+    signal addr_t : natural range 0 to 31 := 0;
+    signal data_in : word_t;
+    signal write_enable : std_logic_vector((N_BYTES - 1) downto 0) := (others => '0');
+    signal data_out : word_t;
 
     signal success : boolean := true;
 begin
-    ram : data_memory generic map(data_width => 32, ADDR_WIDTH => addr_width)
+    ram : data_memory generic map(BYTE_SIZE => byte_size, N_BYTES => n_bytes, ADDR_WIDTH => addr_width)
     port map(clk => clk_t, addr => addr_t, data => data_in, we => write_enable, q => data_out);
 
     clk_t <= not clk_t after 5 ns;
 
     process
-        variable data_int : integer;
+        procedure check(value : boolean; message : string := "") is begin
+            if not value then
+                success <= false;
+                assert false report message severity error;
+            end if;
+        end procedure;
+
+        procedure check_word(addr : natural range 0 to 31; expected_word : word_t) is
+            variable data_int : integer;
+            variable expected_byte : integer;
+        begin
+            addr_t <= addr;
+            wait for 1 ns;
+            for i in 0 to 3 loop
+                data_int := to_integer(unsigned(data_out(BYTE_SIZE * (i + 1) - 1 downto BYTE_SIZE * i)));
+                expected_byte := to_integer(unsigned(expected_word(BYTE_SIZE * (i + 1) - 1 downto BYTE_SIZE * i)));
+                check(expected_byte = data_int, "unexpected value " & integer'image(data_int) & " at " & natural'image(addr_t + i) & ": expected value is " & integer'image(expected_byte));
+            end loop;
+            wait for 9 ns;
+        end procedure;
+
     begin
         wait for 5 ns;
         for addr_pos in 0 to 7 loop
-            addr_t <= natural(addr_pos);
-            data_int := to_integer(unsigned(data_out));
-            if addr_t /= data_int then
-                success <= false;
-            end if;
-            assert addr_t = data_int report "unexpected value " & integer'image(data_int) & " at " & natural'image(addr_t) severity error;
+            check_word(addr_pos * 4,
+            std_logic_vector(to_unsigned(addr_pos * 4 + 3, byte_size)) &
+            std_logic_vector(to_unsigned(addr_pos * 4 + 2, byte_size)) &
+            std_logic_vector(to_unsigned(addr_pos * 4 + 1, byte_size)) &
+            std_logic_vector(to_unsigned(addr_pos * 4 + 0, byte_size))
+            );
+
+        end loop;
+
+        write_enable <= "1111";
+        for addr_pos in 0 to 7 loop
+            addr_t <= natural(addr_pos * 4);
+            data_in <= std_logic_vector(to_unsigned(31 - addr_pos * 4 - 3, byte_size)) &
+                std_logic_vector(to_unsigned(31 - addr_pos * 4 - 2, byte_size)) &
+                std_logic_vector(to_unsigned(31 - addr_pos * 4 - 1, byte_size)) &
+                std_logic_vector(to_unsigned(31 - addr_pos * 4 - 0, byte_size));
             wait for 10 ns;
         end loop;
 
-        write_enable <= '1';
-        for addr_pos in 0 to 2 ** addr_width - 1 loop
-            addr_t <= natural(addr_pos);
-            data_in <= std_logic_vector(to_unsigned(7 - natural(addr_pos), data_width));
-            wait for 10 ns;
-        end loop;
-
-        write_enable <= '0';
-        for addr_pos in 0 to 2 ** addr_width - 1 loop
-            addr_t <= natural(addr_pos);
-            data_int := to_integer(unsigned(data_out));
-            if data_int /= 7 - addr_t then
-                success <= false;
-            end if;
-            assert data_int = 7 - addr_t report "unexpected value " & integer'image(data_int) & " at " & natural'image(addr_t) severity error;
-            wait for 10 ns;
+        write_enable <= "0000";
+        for addr_pos in 0 to 7 loop
+            check_word(addr_pos * 4,
+            std_logic_vector(to_unsigned(31 - addr_pos * 4 - 3, byte_size)) &
+            std_logic_vector(to_unsigned(31 - addr_pos * 4 - 2, byte_size)) &
+            std_logic_vector(to_unsigned(31 - addr_pos * 4 - 1, byte_size)) &
+            std_logic_vector(to_unsigned(31 - addr_pos * 4 - 0, byte_size))
+            );
         end loop;
         if success then
             report "testbench data_memory_tb succesful!";
